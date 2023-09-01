5f821e4e398ba877288926ba4bed523c19ba1a07
HeeCheol Jeong <70684380+JengHC@users.noreply.github.com>
2023-09-01 10:02:23 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,18 @@
+#include <iostream> //입출력 헤더파일
+
+using namespace std;
+
+int main()
+{
+    int A ;
+    int B ;
+
+    //cin >> (입력) cout << (출력)
+    cin >> A;
+    cin >> B;
+
+    cout << A+B << endl;
+
+    return 0;
+}
+
