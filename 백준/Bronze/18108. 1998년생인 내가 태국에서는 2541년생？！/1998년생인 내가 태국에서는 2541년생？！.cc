e9c99bde34679e7e4a1895ab925ebe2f9a56373d
HeeCheol Jeong <70684380+JengHC@users.noreply.github.com>
2023-09-01 10:20:46 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,15 @@
+#include <iostream> //입출력 헤더파일
+using namespace std;
+
+int main()
+{
+    int year;
+
+    //cin >> (입력) cout << (출력)
+    cin >> year;
+
+    cout << year-543 << endl;
+
+    return 0;
+}
+
