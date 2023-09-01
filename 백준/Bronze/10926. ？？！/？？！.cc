ab29430d5cd53cfba21f0cb3857dc188ea3392c5
HeeCheol Jeong <70684380+JengHC@users.noreply.github.com>
2023-09-01 10:09:16 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,16 @@
+#include <iostream> //입출력 헤더파일
+using namespace std;
+
+int main()
+{
+    char a[51]; // char 길이가 50자 까지니까 크기 조절
+
+    //cin >> (입력) cout << (출력)
+    cin >> a;
+
+    cout << a << "??!" << endl;
+
+
+    return 0;
+}
+
