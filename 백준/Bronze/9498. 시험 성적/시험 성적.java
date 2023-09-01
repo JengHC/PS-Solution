bf7ba3958e465a298d81ac1379c02ea6b5d1a82d
HeeCheol Jeong <70684380+JengHC@users.noreply.github.com>
2023-09-01 10:07:10 +0900

    [Bronze V] Title: 시험 성적, Time: 208 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+		public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);		
+		int a;	
+		String c;
+		
+		a=sc.nextInt();
+		
+		if(a>=90) 
+			c="A";
+		else if(a>=80)
+			c="B";
+		else if(a>=70)
+			c="C";
+		else if(a>=60)
+			c="D";
+		else 
+			c="F";
+		System.out.print(c);
+	}
+}
