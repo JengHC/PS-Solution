40a5755a4d2dccb5706495c58214b55a38e5179d
HeeCheol Jeong <70684380+JengHC@users.noreply.github.com>
2024-04-22 19:06:12 +0900

    [D1] Title: 중간값 찾기, Time: 7 ms, Memory: 13,556 KB -BaekjoonHub

--- SWEA/D1/2063. 중간값 찾기/중간값 찾기.cpp
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.cpp
@@ -12,19 +12,26 @@ int main(int argc, char** argv)
 
 	cin >> T;
 
-	// 
-	vector<int> arr;
+	// vector 선언 - vector <[data type]> [변수이름]
+	vector<int> arr;  // 비어있는 vector arr를 생성합니다.
+
+	// ex 1.) vector<int> arr(5);   => 기본값을 0 으로 초기화 된 5개의 원소를 가지는 vector arr 를 생성
+	// ex 2.) vector<int> arr(5,2); => 2로 초기화된 5개의 원소를 가지는 vector arr를 생성
+	// ex 3.) vector<int> arr2(arr1);   => arr2는 arr1 vector를 복사해서 생성
+
 
 	for (test_case = 1; test_case <= T; ++test_case)
 	{
 		int a;
 		cin >> a;
 
-		// 
+		// 마지막 원소 뒤에 원소a를 삽입합니다.
 		arr.push_back(a);
 
 	}
-	sort(arr.begin(), arr.end());
+	sort(arr.begin(), arr.end()); 
+	// arr.begin() 첫번째 원소를 가리킴
+	// arr.end()   마지막의 "다음"을 가리킴 
 
 	cout << arr[T / 2];
 
