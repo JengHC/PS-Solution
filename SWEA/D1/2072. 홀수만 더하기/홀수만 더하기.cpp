db9eeae458bfe9123b01b091d7949ea79a6657e0
HeeCheol Jeong <70684380+JengHC@users.noreply.github.com>
2024-04-22 13:24:26 +0900

    [D1] Title: 홀수만 더하기, Time: 8 ms, Memory: 13,336 KB -BaekjoonHub

--- SWEA/D1/2072. 홀수만 더하기/홀수만 더하기.cpp
+++ SWEA/D1/2072. 홀수만 더하기/홀수만 더하기.cpp
@@ -4,19 +4,21 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
+    //10개의 수를 입력 받아, 그 중 홀 수만 더하는 값 출력
 	int test_case;
-	int T;
-	cin>>T;
+	int T;     
+	cin>>T;   // 값을 입력 받고,
     
 	for(test_case = 1; test_case <= T; ++test_case)
 	{
-        int sum = 0;
-        for (int i = 0; i<10;i++)
+        int sum = 0;                        // 더한값을 받기 위해 sum 변수 
+        
+        for (int i = 0; i<10;i++)          // 테스트 케이스의 첫 줄에 10개가 주어지니까, 0부터 10까지
         {
-            int num = 0;
+            int num = 0;        
+            cin>>num;                      // 더해야 할 값을 입력 받아야함
             
-            cin>>num;
-            if(num%2==1)
+            if(num%2==1)                 // num값이 2로 나눴을때 1이 남으면 홀수니까, 그 값을 더해서 sum으로, 
                 sum += num;
         }
         
