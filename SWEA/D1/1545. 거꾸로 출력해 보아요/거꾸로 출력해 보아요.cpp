c58fbf033dd9d121a1c46d9795772681a73047a6
HeeCheol Jeong <70684380+JengHC@users.noreply.github.com>
2024-04-30 10:37:53 +0900

    [D1] Title: 거꾸로 출력해 보아요, Time: 6 ms, Memory: 13,336 KB -BaekjoonHub

---
+++ SWEA/D1/1545. 거꾸로 출력해 보아요/거꾸로 출력해 보아요.cpp
@@ -0,0 +1,16 @@
+// swea1545.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = n; i >= 0; i--)
+    {
+        cout << i << " ";
+    }
+    return 0;
+}
