737010697eb96712327a7f744f6fe7a0c5692e88
HeeCheol Jeong <70684380+JengHC@users.noreply.github.com>
2024-04-30 09:03:41 +0900

    [D1] Title: 1대1 가위바위보, Time: 6 ms, Memory: 13,540 KB -BaekjoonHub

--- SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.cpp
+++ SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.cpp
@@ -6,17 +6,13 @@ int main(int argc, char** argv)
 	int a,b;
 	cin >> a >> b;
 
-	if (a > b)
+	if(a-b ==1 || a-b==2)
 	{
-		cout << "A" << endl;
+		cout<< "A" <<endl;
 	}
-	else if (a < b)
+	else 
 	{
-		cout << "B" << endl;
-	}
-	else
-	{
-		NULL;
+		cout<< "B" <<endl;
 	}
 
 	return 0;
