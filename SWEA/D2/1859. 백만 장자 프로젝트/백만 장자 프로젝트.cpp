7f87c030616df863c4ddc5a12250f693efe6ce50
HeeCheol Jeong <70684380+JengHC@users.noreply.github.com>
2024-05-03 17:19:07 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 1,661 ms, Memory: 17,448 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.cpp
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.cpp
@@ -3,7 +3,7 @@
 #define INT long long
 using namespace std;
 
-int arr[1000001], N;
+int arr[1000001], N; // 배열과 변수 선언
 
 int main(int argc, char** argv)
 {
@@ -14,25 +14,25 @@ int main(int argc, char** argv)
 
 	for (test_case = 1; test_case <= T; ++test_case)
 	{
-		int max = 0;
-		long long sum = 0;
+		int max = 0;       // 최대 매매가 초기화
+		long long sum = 0; // 이익의 합 초기화
 
 		cin >> N;
-		for (int i = 1; i<=N; i++)
+		for (int i = 1; i<=N; i++)  // 각 날짜의 매매가 입력
 		{
 			cin >> arr[i];
 		}
-		max = arr[N];
+		max = arr[N];               // 최대 매매가 초기화
 
-		for (int i = N - 1; i > 0; i--)
+		for (int i = N - 1; i > 0; i--) // 역순으로 각 날짜에 대해
 		{
-			if (max < arr[i])
+			if (max < arr[i])           // 최대 매매가 갱신
 			{
 				max = arr[i];
 			}
-			else
+			else                        // 최대 매매가를 넘지 못하면
 			{
-				sum += max - arr[i];
+				sum += max - arr[i];    // 이익을 더함
 			}
 		}
 		cout << "#" << test_case << " " << sum << endl;
