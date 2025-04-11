ab1815358f5b8fa44cf408338722f0543adf68a1
HeeCheol Jeong <70684380+JengHC@users.noreply.github.com>
2025-04-11 13:35:25 +0900

    Title: 변수 값 복사하기 3, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250411/변수 값 복사하기 3/copying-variable-values-3.cpp
+++ 250411/변수 값 복사하기 3/copying-variable-values-3.cpp
@@ -2,13 +2,11 @@
 using namespace std;
 
 int main() {
-    int a=1;
-    int b=5;
-    int c=3;
+    int a=1, b=5, c=3;
 
-    int a = c;
-    int a=a+c;
-    int b = b-c;
+    a = c;
+    a=a+c;
+    b = b-c;
 
     cout<< a << endl;
     cout<< b <<endl;
