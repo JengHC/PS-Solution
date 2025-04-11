1824fbac68f6351c6c28eabef143d09f9ac4a1a4
HeeCheol Jeong <70684380+JengHC@users.noreply.github.com>
2025-04-11 13:18:06 +0900

    Title: 2줄 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250411/2줄 출력/print-two-lines.cpp
+++ 250411/2줄 출력/print-two-lines.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     // Please write your code here.
-    cout<<"Hello"\n;
-    cout<<"World"\n;
+    cout<<"Hello"<<\n;
+    cout<<"World"<<\n;
     return 0;
 }
