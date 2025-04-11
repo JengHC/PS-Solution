923ce68c9f844f46b686685d1d0e6aa0fc651be9
HeeCheol Jeong <70684380+JengHC@users.noreply.github.com>
2025-04-11 14:08:17 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250411/달에서 무게 구하기/weight-on-the-moon.cpp
+++ 250411/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -6,7 +6,8 @@ int main() {
     double gravityScale = 0.165;   
     double wg = weight * gravityScale;
 
-    cout << fixed << setprecision(6);
+    cout<<fixed;
+    cout.precision(6);    
     cout << weight << " * " << gravityScale << " = " << wg << endl;
 
     return 0;
