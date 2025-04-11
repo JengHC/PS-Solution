0df7aa6f903423fbb71c2761185d98da360bf73d
HeeCheol Jeong <70684380+JengHC@users.noreply.github.com>
2025-04-11 15:05:39 +0900

    Title: 합과 평균, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250411/합과 평균/sum-and-avg.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A,B;
+
+    cin>>A>>B;
+
+    cout<<fixed;
+    cout.precision(1);
+
+    cout<<A+B<< " " << A/B <<endl;
+
+    return 0;
+}
