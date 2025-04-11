72e98139807a147c9593cff3420edd9e3dfad460
HeeCheol Jeong <70684380+JengHC@users.noreply.github.com>
2025-04-11 13:34:21 +0900

    Title: 변수 값 복사하기 3, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250411/변수 값 복사하기 3/copying-variable-values-3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=1;
+    int a=5;
+    int c=3;
+
+    int a = c;
+    int a=a+c;
+    int b = b-c;
+    
+    cout<< a <<\n;
+    cout<< b <<\n;
+    cout<< c <<\n;
+    return 0;
+}
