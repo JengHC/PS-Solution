fea9fc1d64d7a3726f856019de61d865b4e3afaf
HeeCheol Jeong <70684380+JengHC@users.noreply.github.com>
2025-04-14 16:48:43 +0900

    Title: 특정 조건 두 정수 비교, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250414/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A,B;
+    cin>>A>>B;
+
+    if(B>A)
+    {
+        cout<<1<<" " ;
+    }
+    else
+    {
+        cout<<0<<" ";
+    }
+    if(B==A)
+    {
+        cout<<1<<" ";
+    }
+    else
+    {
+        cout<<0<<" ";
+    }
+    return 0;
+}
