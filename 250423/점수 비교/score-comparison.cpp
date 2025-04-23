d3f545db73c9ca36acc20e6591b83aba1ed759ed
HeeCheol Jeong <70684380+JengHC@users.noreply.github.com>
2025-04-23 17:39:32 +0900

    Title: 점수 비교, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250423/점수 비교/score-comparison.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A1,A2,B1,B2;
+    cin>>A1>>A2;
+    cin>>B1>>B2;
+
+    if(A1>B1 && A2>B2)
+    {
+        cout<< 1 <<endl;
+    }
+    else
+    {
+        cout<<0<<endl;
+    }
+    return 0;
+}
